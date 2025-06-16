87e05f60e64eee78b52e43362e3e4aadc02923fc
EduardoFF <efeoflus@andrew.cmu.edu>
2025-06-16 14:58:14 +0300

    first commit

---
+++ fizzbuzz.py
@@ -0,0 +1,3 @@
+""" Edit the function below to implement the FizzBuzz TDD Kata """
+def fizzbuzz(n):
+    return "42"
---
+++ test_simple.py
@@ -0,0 +1,5 @@
+from fizzbuzz import fizzBuzz
+
+
+# write your tests here
+# call fizzBuzz to test your function
